87b0d28bf468a8bf3a79a6a7a25c81942983a1fd
root <root@WSTEMFA39-13.stu.net.fr.ch>
2024-04-19 14:30:04 +0200

    test

--- projet/Rest_1/src/main/java/com/example/rest1/Rest1Application.java
+++ projet/Rest_1/src/main/java/com/example/rest1/Rest1Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Rest1Application {
-
+//test
 	public static void main(String[] args) {
 		SpringApplication.run(Rest1Application.class, args);
 	}
