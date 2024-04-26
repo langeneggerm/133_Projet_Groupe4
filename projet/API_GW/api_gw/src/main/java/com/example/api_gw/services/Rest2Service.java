6ee62fa5174bcf4349ffd7dd17d3875922d824de
MrPepinou <g.nevantonio@gmail.com>
2024-04-26 16:31:31 +0200

    rest2Service fini. Controller partie admin finie

---
+++ projet/API_GW/api_gw/src/main/java/com/example/api_gw/services/Rest2Service.java
@@ -0,0 +1,84 @@
+package com.example.api_gw.services;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import jakarta.transaction.Transactional;
+
+public class Rest2Service{
+    private final RestTemplate restTemplate = new RestTemplate();
+    private final static String restUrl = "http://localhost:8080"; //à changer quand mis en place
+    
+    public Rest2Service(){
+    }
+
+    @Transactional
+    public ResponseEntity<String> addNewCommande(Date date, int idNerf, int user, double prix) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("date", date);
+        body.put("fk_nerf", idNerf);
+        body.put("fk_user", idNerf);
+        body.put("montant", prix);
+        ResponseEntity<String> jsonResponse = restTemplate.postForEntity(restUrl + "/acheterNerf", body, String.class);
+        return jsonResponse;
+    }
+    //trouver comment faire pour envoyer un body avec restTemplate
+
+    @Transactional
+    public ResponseEntity<String> getUser(int id) {
+        ResponseEntity<String> jsonResponse = restTemplate.getForEntity(restUrl + "/getUser?pk_user=" + id, String.class);
+        return jsonResponse;
+    }
+
+    @Transactional
+    public ResponseEntity<String> changerSolde(int id, double prix) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("pk_user", id);
+        body.put("montant", prix);
+        ResponseEntity<String> jsonResponse = restTemplate.postForEntity(restUrl + "/changeSolde", body, String.class);
+        return jsonResponse;
+    }
+
+    @Transactional
+    public ResponseEntity<String> changerPrixNerf(int id, double newPrice) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("id", id);
+        body.put("prix", newPrice);
+        ResponseEntity<String> jsonResponse = restTemplate.postForEntity(restUrl + "/newPrice", body, String.class);
+        return jsonResponse;
+    }
+
+    @Transactional
+    public ResponseEntity<String> findAllCommandes(int id) {
+        Map<String, Integer> body = new HashMap<>();
+        body.put("pk_user", id);
+        ResponseEntity<String> jsonResponse = restTemplate.postForEntity(restUrl + "/getCommande", body, String.class);
+        return jsonResponse;
+    }
+
+    @Transactional
+    public ResponseEntity<String> login(int id, String mdp) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("pk_user", id);
+        body.put("password", mdp);
+        ResponseEntity<String> jsonResponse = restTemplate.postForEntity(restUrl + "/login", body, String.class);
+        return jsonResponse;
+    }
+
+    @Transactional
+    public ResponseEntity<String> getHash(String mdp) {
+        Map<String, String> body = new HashMap<>();
+        body.put("password", mdp);
+        ResponseEntity<String> jsonResponse = restTemplate.postForEntity(restUrl + "/mdphash", body, String.class);
+        return jsonResponse;
+    }
+
+    
+
+
+
+}
--- projet/API_GW/api_gw/src/main/java/controller/Controller.java
+++ projet/API_GW/api_gw/src/main/java/controller/Controller.java
@@ -1,6 +1,10 @@
 package controller;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -8,8 +12,97 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.api_gw.services.Rest1Service;
+import com.example.api_gw.services.Rest2Service;
+
+import jakarta.servlet.http.HttpSession;
+
 
 @RestController
 public class Controller {
+
+    private final Rest1Service rest1;
+    private final Rest2Service rest2;
     
+    @Autowired
+    public Controller(Rest1Service rest1, Rest2Service rest2){
+        this.rest1 = rest1;
+        this.rest2 = rest2;
+    };
+
+    @GetMapping("/getAllNerf")
+    public @ResponseBody String getAllNerf() {
+        return rest1.getAllNerf();
+    }
+
+    @GetMapping("/getNerf")
+    public @ResponseBody ResponseEntity<String> getNerf(@RequestParam int id) {
+        return rest1.getNerf(id);
+    }
+
+    @PutMapping("/newStock")
+    public @ResponseBody ResponseEntity<String> fullStock(HttpSession session, @RequestParam int id, @RequestParam int addedQty) {
+        ResponseEntity<String> result = null;
+        if(session.getAttribute("login") != null){
+            if(session.getAttribute("isAdm").equals(1)){
+                result = rest1.fullNerfStock(id, addedQty);
+            } else {
+                HttpStatus httpCode = HttpStatus.UNAUTHORIZED;
+                result = new ResponseEntity<>("Only administrators are authorized to change the store.", httpCode);
+            }
+        } else {
+            HttpStatus httpCode = HttpStatus.UNAUTHORIZED;
+            result = new ResponseEntity<>("You are not logged in. Try again when logged!", httpCode);
+        }
+        return result;
+    }
+
+    @PostMapping("/addNerf")
+    public @ResponseBody ResponseEntity<String> addNerf(HttpSession session, @RequestParam String nom, @RequestParam String description, @RequestParam String typeTir, @RequestParam double prix, @RequestParam int quantite, @RequestParam byte[] img) {
+        ResponseEntity<String> result = null;
+        if(session.getAttribute("login") != null){
+            if(session.getAttribute("isAdm").equals(1)){
+                result = rest1.addNewNerf(nom, description, typeTir, prix, quantite, img);
+            } else {
+                HttpStatus httpCode = HttpStatus.UNAUTHORIZED;
+                result = new ResponseEntity<>("Only administrators are authorized to change the store.", httpCode);
+            }
+        } else {
+            HttpStatus httpCode = HttpStatus.UNAUTHORIZED;
+            result = new ResponseEntity<>("You are not logged in. Try again when logged!", httpCode);
+        }
+        return result;
+    }
+
+    @PutMapping("/newPrice")
+    public @ResponseBody ResponseEntity<String> changerPrix(HttpSession session, @RequestParam int id, @RequestParam double prix) {
+        ResponseEntity<String> result = null;
+        if(session.getAttribute("login") != null){
+            if(session.getAttribute("isAdm").equals(1)){
+                result = rest1.changerPrixNerf(id, prix);
+            } else {
+                HttpStatus httpCode = HttpStatus.UNAUTHORIZED;
+                result = new ResponseEntity<>("Only administrators are authorized to change the store.", httpCode);
+            }
+        } else {
+            HttpStatus httpCode = HttpStatus.UNAUTHORIZED;
+            result = new ResponseEntity<>("You are not logged in. Try again when logged!", httpCode);
+        }
+        return result;
+    }
+
+    @PostMapping("/achatNerf")
+    public @ResponseBody ResponseEntity<String> achatNerf(HttpSession session, @RequestParam Date date, @RequestParam int idNerf, @RequestParam int idUser, @RequestParam double prix) {
+        ResponseEntity<String> result = null;
+        if(session.getAttribute("login") != null){
+                rest2.getUser(idUser).getBody();
+        } else {
+            HttpStatus httpCode = HttpStatus.UNAUTHORIZED;
+            result = new ResponseEntity<>("You are not logged in. Try again when logged!", httpCode);
+        }
+        return result;
+    }
+
+
+
 }
